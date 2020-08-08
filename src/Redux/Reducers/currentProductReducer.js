fe45dfa669041bf02f7d34960f1c622336c3a1a0
Trevor Mackin <tismetrm@gmail.com>
2020-08-08 16:31:05 -0700

    Add a currentProductReducer that will update the currentProduct slice based on a product that a user has selected

---
+++ src/Redux/Reducers/currentProductReducer.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+export const currentProductSlice = createSlice({
+    name: 'currentProduct',
+    initialState: {
+        value: 1001,
+    },
+    reducers: {
+            // Redux Toolkit allows us to write "mutating" logic in reducers. It
+            // doesn't actually mutate the state because it uses the Immer library,
+            // which detects changes to a "draft state" and produces a brand new
+            // immutable state based off those changes
+        selectProduct: (state, action) => {
+            console.log("I am the payload: ", action.payload);
+            state.value = action.payload;
+        },
+    },
+});
+
+
+export const { selectProduct } = currentProductSlice.actions;
+
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state) => state.counter.value)`
+export const getCurrentProduct = state => state.currentProduct.value;
+
+
+export default currentProductSlice.reducer;

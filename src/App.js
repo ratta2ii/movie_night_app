9df9f54574af9fdff510be29e7b3e068904145d1
Trevor Mackin <tismetrm@gmail.com>
2020-08-09 12:16:13 -0700

    Update state expamples to work with a string instead of an object

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 // This is here to explore the flow of redux in this template
-import { Counter } from './features/counter/Counter';
 import { ProductList } from './Components/ProductList/ProductList';
 
 

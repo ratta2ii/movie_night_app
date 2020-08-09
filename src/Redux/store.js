8f7dac76a7462264ca870353e5eca5f29a57b57f
Trevor Mackin <tismetrm@gmail.com>
2020-08-08 22:57:45 -0700

    Remove counter slice, the reducer, and the features directory with the Counter component

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import currentProductReducer from './Reducers/currentProductReducer';
 import currentWishListReducer from './Reducers/currentWishListReducer';
 
 
 export default configureStore({
     reducer: {
-        counter: counterReducer,
         currentProduct: currentProductReducer,
         currentWishList: currentWishListReducer,
     },

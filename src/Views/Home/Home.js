43372a63ff8eac599f25f004610ffba1822ddb1d
Trevor Mackin <tismetrm@gmail.com>
2020-08-09 13:05:59 -0700

    Layout project structure in a directory named Views

---
+++ src/Views/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Home(props) {
+    
+
+    return (
+        <div>
+            <h2>I am the homepage!</h2>
+        </div>
+    )
+}
+
+export default Home;

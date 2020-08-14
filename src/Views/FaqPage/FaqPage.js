49c9d36f07046cefa703b93c050289c49e7d3e93
Trevor Mackin <tismetrm@gmail.com>
2020-08-13 23:52:21 -0700

    Add a Faqpage view that will display all FAQ's Style the Faqpage view for all viewport sizes, including mobile

---
+++ src/Views/FaqPage/FaqPage.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import Faqs from './../../Components/Faqs/Faqs';
+import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
+import useStyles from './FaqPageStyles';
+import BookOnlineButton from './../../Assets/Images/book-online-button.png';
+
+
+const FaqPage = (props) => {
+
+
+    console.log("I am FaqPage props: ", props);
+    const classes = useStyles();
+
+
+    return (
+        <Box>
+            <Grid container>
+                {/* Header left margin */}
+                <Grid item xs={0} sm={1} md={2}></Grid>
+                {/* Header Grid Container */}
+                <Grid item xs={12} sm={10} md={8}>
+                    <Box className={classes.headerContainer} >
+                        <Box className={classes.headerOverlayContainer} >
+                            <Typography variant="h3" className={classes.productHeaderTitle} >
+                                FAQ'S
+                            </Typography>
+                            <Link to="/reservations">
+                                <img src={BookOnlineButton}
+                                    alt='book movie theatre rental online now button'
+                                    className={classes.bookOnlineButton} />
+                            </Link>
+                        </Box>
+                    </Box>
+                </Grid>
+                {/* Header right margin */}
+                <Grid item xs={0} sm={1} md={2}></Grid>
+
+
+                {/* Faqs container margin left */}
+                <Grid item xs={0} sm={1} md={2}></Grid>
+                {/* MAIN FAQS CONTAINER */}
+                <Grid item xs={12} sm={10} md={8} >
+                    <Box className={classes.faqsContainer}>
+                        <Box className={classes.headerOverlayContainer} >
+                            <Faqs />
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
+    )
+}
+
+
+FaqPage.propTypes = {
+
+}
+
+
+export default FaqPage;
---
+++ src/Views/FaqPage/FaqPageStyles.js
@@ -0,0 +1,72 @@
+import { makeStyles } from '@material-ui/core/styles';
+import header_bg_image from './../../Assets/Images/film4.jpg';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        backgroundColor: "",
+        diplay: 'flex'
+    },
+    headerContainer: {
+        height: 175,
+        background: `url('${header_bg_image}') no-repeat`,
+        backgroundSize: '100%',
+        backgroundColor: '#2196f326',
+        marginBottom: '50px',
+        borderRadius: '3px',
+        boxShadow: '0px 0px 5px #161f3a',
+        [theme.breakpoints.down('sm')]: {
+            margin: '175px auto 50px auto'
+        },
+        [theme.breakpoints.down('xs')]: {
+            width: '90%',
+            marginTop: 150
+        },
+    },
+    headerOverlayContainer: {
+        height: '100%',
+        width: '100%',
+        backgroundColor: '#7b7b7b30',
+        borderRadius: '3px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative'
+    },
+    productHeaderTitle: {
+        fontSize: '7rem',
+        fontFamily: '"Roboto", "Helvetica", "Arial"',
+        fontWeight: 800,
+        lineHeight: 1.167,
+        letterSpacing: '0.02em',
+        color: '#00000024',
+        '&:focus': {
+            
+        },
+        '&:hover': {
+            
+        },
+        '&:active': {
+
+        },
+        [theme.breakpoints.down('sm')]: {
+            display: 'none',
+        },
+    },
+    bookOnlineButton: {
+        position: 'absolute',
+        width: 130,
+        bottom: 12,
+        right: 14,
+    },
+    faqsContainer: {
+        padding: 25,
+        borderRadius: '3px',
+        boxShadow: '0px 0px 5px #161f3a',
+        background: 'rgb(104,123,127)',
+        background: 'linear-gradient(90deg, rgba(104,123,127,1) 0%, rgba(89,106,109,1) 40%, rgba(74,76,77,1) 100%)',
+    }
+}));
+
+
+export default useStyles;

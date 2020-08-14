d6267d8afee7f786acf752471794f6a272fdcd78
Trevor Mackin <tismetrm@gmail.com>
2020-08-14 00:05:11 -0700

    Add styling to ProductsView component

--- src/Views/ProductsView/ProductsView.js
+++ src/Views/ProductsView/ProductsView.js
@@ -1,13 +1,52 @@
 import React from 'react'
+import ProductList from './../../Components/ProductList/ProductList';
+import propTypes from 'prop-types';
+import { Link } from "react-router-dom";
+import useStyles from './ProductsViewStyles';
+import { Box } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import BookOnlineButton from './../../Assets/Images/book-online-button.png';
+
+
+
 
 
 function ProductView(props) {
+
     
+    const classes = useStyles();
+
 
     return (
-        <div>
-            <h2>I am the productView!</h2>
-        </div>
+        <Box>
+            <Grid container>
+                {/* Header left margin */}
+                <Grid item xs={0} sm={1} md={2}></Grid>
+                {/* Header Grid Container */}
+                <Grid item xs={12} sm={10} md={8}>
+                    <Box className={classes.headerContainer} >
+                        <Box className={classes.headerOverlayContainer}
+                            style={{
+                            }}>
+                            <Typography variant="h3" className={classes.productHeaderTitle}>
+                                PRODUCTS
+                        </Typography>
+                            <Link to="/reservations">
+                                <img src={BookOnlineButton}
+                                    alt='book movie theatre rental online now button'
+                                    className={classes.bookOnlineButton} />
+                            </Link>
+
+                        </Box>
+                    </Box>
+                </Grid>
+                {/* Header right margin */}
+                <Grid item xs={0} sm={1} md={2}></Grid>
+                {/* MAIN PRODUCT CONTAINER */}
+                <ProductList />
+            </Grid>
+        </Box>
     )
 }
 
---
+++ src/Views/ProductsView/ProductsViewStyles.js
@@ -0,0 +1,65 @@
+import { makeStyles } from '@material-ui/core/styles';
+import bg_image from './../../Assets/Images/film20-cropped.jpg';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        backgroundColor: "",
+        diplay: 'flex'
+    },
+    headerContainer: {
+        height: 175,
+        background: `url('${bg_image}') no-repeat`,
+        backgroundSize: '100%',
+        backgroundColor: '#2196f326',
+        marginBottom: '50px',
+        borderRadius: '3px',
+        boxShadow: '0px 0px 5px #161f3a',
+        [theme.breakpoints.down('sm')]: {
+            margin: '175px auto 50px auto'
+        },
+        [theme.breakpoints.down('xs')]: {
+            width: '90%',
+            marginTop: 150
+        },
+    },
+    headerOverlayContainer: {
+        height: '100%',
+        width: '100%',
+        backgroundColor: '#7b7b7b30',
+        borderRadius: '3px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative'
+    },
+    productHeaderTitle: {
+        fontSize: '6rem',
+        fontFamily: '"Roboto", "Helvetica", "Arial"',
+        fontWeight: 800,
+        lineHeight: 1.167,
+        letterSpacing: '0.02em',
+        color: '#00000024',
+        '&:focus': {
+            
+        },
+        '&:hover': {
+            
+        },
+        '&:active': {
+
+        },
+        [theme.breakpoints.down('sm')]: {
+            display: 'none',
+        },
+    },
+    bookOnlineButton: {
+        position: 'absolute',
+        width: 120,
+        bottom: 12,
+        right: 14,
+    },
+}));
+
+
+export default useStyles;

fd1aa0dd2247e6164548d85f2b448017b0303879
Trevor Mackin <tismetrm@gmail.com>
2020-08-16 02:07:09 -0700

    Update the addWish and removeWish reducer Change functionality in addWish so that will check for duplicates before adding a project Write functionality to remove an item from the state slice when an action is dispatched

--- src/Redux/Reducers/currentWishListReducer.js
+++ src/Redux/Reducers/currentWishListReducer.js
@@ -4,51 +4,31 @@ import { createSlice } from '@reduxjs/toolkit';
 export const currentWishListSlice = createSlice({
     name: 'currentWishList',
     initialState: {
-        value: [],
+        value: {},
     },
     reducers: {
-        // Redux Toolkit allows us to write "mutating" logic in reducers. It
-        // doesn't actually mutate the state because it uses the Immer library,
-        // which detects changes to a "draft state" and produces a brand new
-        // immutable state based off those changes
         addWish: (state, action) => {
-
-            console.log("Made it into wish list reducer");
-            console.log("Wishlist action: ", action);
-            console.log("State Value: ", state.value);
-            
-            let newWish = action.payload;
-            
-            //? Avoid adding the same product twice (Other possible solutions???) ?\\
-            // if (state.value.length > 0) {
-                //     state.value.forEach(ele => {
-                    //         if (ele.productId !== action.payload.productId) {
-                        //             newWish = action.payload;
-                        //         }
-                        //     })
-                        // }
-                        
-                        state.value = [
-                            ...state.value.slice(0),
-                            newWish
-                        ];
-                        
-            console.log("State Value Ending: ", state.value);
-            // This is an alternative way to do this
-            // const newWishList = [ ...state.value, action.payload ]; 
-            // state.value = newWishList;
+            //! Remove console statements
+            // console.log("Made it into addWish reducer");
+            //* The payload here is an object containing all the wish properties
+            const newState = Object.assign({}, state.value, {
+                [action.payload.productId]: {
+                    ...action.payload
+                }
+            });
+            //? Redux Toolkit allows us to write "mutating" logic in reducers. It doesn't mutate the state. It uses the Immer library, which detects changes to a "draft state" and produces a brand new immutable state based off those changes 
+            state.value = newState;
+            //! Fucntionality for working with an array state slice instead of an object !\\
+            // let newWish = action.payload;
+            // state.value = [ ...state.value.slice(0), newWish ];
         },
         removeWish: (state, action) => {
-            console.log("Payload in removeWish: ", action.payload);
-            // This will needed to tested once data is added
-            // let wishList = state.value;
-            // const productIdToRemove = action.payload;
-            // for (let i in wishList) {
-            //     if (wishList[i].id === productIdToRemove) {
-            //         wishList.splice(i);
-            //     }
-            // }
-            // state.value = wishList;
+            //! Remove console statement !\\
+            // console.log("Payload in removeWish reducer: ", action.payload);
+            var indx = parseInt(action.payload);
+            const newState = Object.assign({}, state.value);
+            delete newState[indx];
+            state.value = newState;
         },
     },
 });

05a9b18a61322b6ea565259b84d48d895cb45598
Trevor Mackin <tismetrm@gmail.com>
2020-08-16 02:12:06 -0700

    Add and style  a table that will populate a cart (wishlist)

--- src/Components/Wish/WishStyles.js
+++ src/Components/Wish/WishStyles.js
@@ -3,8 +3,23 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        backgroundColor: "green"
-    }
+        backgroundColor: "green",
+        fontSize: '5em',
+    },
+    imageTableContainer: {
+        // display: 'flex',
+        padding: '20 15 15 15',
+        // justifyContent: 'center'
+    },
+    mainImage: {
+        width: 100,
+        height: 80,
+        padding: 15
+    },
+    tableText: {
+        fontSize: 16,
+    },
+
 }));
 
 
--- src/Components/WishList/WishList.js
+++ src/Components/WishList/WishList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles';
@@ -11,8 +12,10 @@ import Box from '@material-ui/core/Box';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 // import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 // State management
-import { removeWish, getCurrentWishList } from '../../Redux/Reducers/currentWishListReducer'
+import { removeWish, addWish, getCurrentWishList } from '../../Redux/Reducers/currentWishListReducer'
 import Wish from '../Wish/Wish';
+
+import * as styles from './WishListStyles';
 
 
 const WishList = props => {
@@ -22,11 +25,18 @@ const WishList = props => {
     const currentWishList = useSelector(getCurrentWishList);
     const classes = useStyles();
     const dispatch = useDispatch();
+    let history = useHistory();
+    console.log(currentWishList);
 
 
     const handleRemoveWish = (productId) => {
         console.log("handleRemoveWish (productId): ", productId);
+        console.log("wishlist", currentWishList);
+
         dispatch(removeWish(productId));
+        setTimeout(() => {
+            history.push("/wishList");
+        }, 1500);
     }
 
 
@@ -37,22 +47,108 @@ const WishList = props => {
                 <Grid item xs={0} ></Grid>
                 {/* Main Container */}
                 <Grid item xs={12} >
-                {currentWishList.map((ele) =>
-                            <Wish
-                                title={ele.title}
-                                productId={ele.productId}
-                                mainImage={ele.mainImage}
-                                price={ele.price}
-                                id={ele.id}
-                                description={ele.description}
-                                bullets={ele.bullets} 
-                                handleRemoveWish={handleRemoveWish} 
-                            />
-                        )}
+
+
+
+
+
+
+                    <table class="redTable">
+                        <thead>
+                            <tr>
+                                <th>Image</th>
+                                <th>Title</th>
+                                <th>Remove</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <td colspan="4">
+                                </td>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+
+
+
+
+
+
+
+
+                            {Object.keys(currentWishList).map(function (productId) {
+                                var ele = currentWishList[productId];
+                                return <Wish
+                                    title={ele.title}
+                                    productId={ele.productId}
+                                    mainImage={ele.mainImage}
+                                    price={ele.price}
+                                    id={ele.id}
+                                    description={ele.description}
+                                    bullets={ele.bullets}
+                                    handleRemoveWish={handleRemoveWish}
+                                />;
+                            })}
+
+
+                        </tbody>
+                    </table>
+
+
                 </Grid>
             </Grid>
+            <style jsx="true">{`
+                table.redTable {
+                    border: 2px solid #59565a;
+                    background-color: #FFFFFF;
+                    width: 100%;
+                    text-align: center;
+                    border-collapse: collapse;
+                }
+                table.redTable td, table.redTable th {
+                    border: 1px solid #59565a;
+                    padding: 3px 2px;
+                }
+                table.redTable tbody td {
+                    font-size: 16px;
+                }
+                table.redTable tr:nth-child(even) {
+                    background: #F5C8BF;
+                }
+                table.redTable thead {
+                    background: #20b8ff;
+                }
+                table.redTable thead th {
+                    font-size: 19px;
+                    font-family: 'Roboto';
+                    font-weight: bold;
+                    color: #FFFFFF;
+                    text-align: center;
+                    border-left: 2px solid #59565a;
+                }
+                table.redTable thead th:first-child {
+                    border-left: none;
+                }
+                
+                table.redTable tfoot {
+                    font-size: 16px;
+                    font-weight: bold;
+                    color: #FFFFFF;
+                    background: #59565a;
+                }
+                table.redTable tfoot td {
+                    font-size: 16px;
+                }
+                table.redTable tfoot .links {
+                    text-align: right;
+                }
+            `}</style>
         </Box>
     )
+
+
+
+
 }
 
 
@@ -62,5 +158,37 @@ WishList.propTypes = {
 
 
 export default withStyles(useStyles)(WishList);
+
+
+
+
+
+// return (
+//     <Box>
+//         <Grid container>
+//             {/* Left margin */}
+//             <Grid item xs={0} ></Grid>
+//             {/* Main Container */}
+//             <Grid item xs={12} >
+
+//                 {Object.keys(currentWishList).map(function (productId) {
+//                     var ele = currentWishList[productId];
+//                     return <Wish
+//                         title={ele.title}
+//                         productId={ele.productId}
+//                         mainImage={ele.mainImage}
+//                         price={ele.price}
+//                         id={ele.id}
+//                         description={ele.description}
+//                         bullets={ele.bullets}
+//                         handleRemoveWish={handleRemoveWish}
+//                     />;
+//                 })}
+
+
+//             </Grid>
+//         </Grid>
+//     </Box>
+// )
 
 

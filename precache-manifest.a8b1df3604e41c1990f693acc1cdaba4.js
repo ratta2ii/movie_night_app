b136a50369187dbfa521a2001b0c73d08ab499d9
Trevor Mackin <tismetrm@gmail.com>
2020-09-14 20:55:45 -0700

    Updates

---
+++ precache-manifest.a8b1df3604e41c1990f693acc1cdaba4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6825ef78d10fbdb9450f679f2c6c8512",
+    "url": "/movie_night_app/index.html"
+  },
+  {
+    "revision": "19be5879599a62995c80",
+    "url": "/movie_night_app/static/css/main.62a21b2f.chunk.css"
+  },
+  {
+    "revision": "f63a8de089d20056d607",
+    "url": "/movie_night_app/static/js/2.26629714.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/movie_night_app/static/js/2.26629714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19be5879599a62995c80",
+    "url": "/movie_night_app/static/js/main.8d5f6b98.chunk.js"
+  },
+  {
+    "revision": "4c787393de4262b8b5bb",
+    "url": "/movie_night_app/static/js/runtime-main.08086021.js"
+  },
+  {
+    "revision": "86b2ec89da40956f1c4f950cfbda8fb3",
+    "url": "/movie_night_app/static/media/bg12copy.86b2ec89.png"
+  },
+  {
+    "revision": "54b4137187bb72a7e46abb7973fb01b0",
+    "url": "/movie_night_app/static/media/family-movie-night.54b41371.jpg"
+  },
+  {
+    "revision": "97cf373a0c682bad0287375e3b8aad3d",
+    "url": "/movie_night_app/static/media/giant-screen.97cf373a.jpg"
+  },
+  {
+    "revision": "ea8931b95dfe4eab4be35b30d106f148",
+    "url": "/movie_night_app/static/media/instagram-icon.ea8931b9.png"
+  },
+  {
+    "revision": "62ad40cf81d4279cfc88c0ed52d22ef6",
+    "url": "/movie_night_app/static/media/menu-icon.62ad40cf.png"
+  },
+  {
+    "revision": "923fe53306d35a0daf3b8375fa9b60a2",
+    "url": "/movie_night_app/static/media/pool-image.923fe533.JPG"
+  },
+  {
+    "revision": "9224ba5e686cbfefe151d6aba3266c2b",
+    "url": "/movie_night_app/static/media/product-cotton-candy.9224ba5e.jpg"
+  },
+  {
+    "revision": "dd4745836b689bf9e42b6a4c065322b5",
+    "url": "/movie_night_app/static/media/product-hotdog.dd474583.png"
+  },
+  {
+    "revision": "6acb1d56eaa79292cb920b0adcd239e2",
+    "url": "/movie_night_app/static/media/product-popcorn5.6acb1d56.jpg"
+  },
+  {
+    "revision": "7e89e16e933f2431ab03594d50ea6ab2",
+    "url": "/movie_night_app/static/media/productTable.7e89e16e.jpg"
+  },
+  {
+    "revision": "27d17deb917b3068afaa5a3d499220ab",
+    "url": "/movie_night_app/static/media/screens.27d17deb.jpg"
+  },
+  {
+    "revision": "53449fd6e919f9b6da280893d00daeff",
+    "url": "/movie_night_app/static/media/sound.53449fd6.jpg"
+  },
+  {
+    "revision": "42fe1fa3eae42b8ceb6bb481e1977eb0",
+    "url": "/movie_night_app/static/media/sunset.42fe1fa3.jpeg"
+  },
+  {
+    "revision": "c8d54fd94c984a1f7793afe0fedec005",
+    "url": "/movie_night_app/static/media/twitter-icon.c8d54fd9.png"
+  }
+]);

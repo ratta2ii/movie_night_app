6aebeeafd2b9d8cbd7ddf3008beb1f47d48ef5b6
Trevor Mackin <tismetrm@gmail.com>
2023-10-04 13:26:48 -0700

    Revert back to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import store from './Redux/store';
@@ -10,12 +10,15 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter> {/* Use BrowserRouter */}
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
